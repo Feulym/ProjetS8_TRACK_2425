0e2d63761974f8abbd97ef290a5632fe08c217e2
Screamnox <92693756+Screamnox@users.noreply.github.com>
2025-01-20 13:35:33 +0100

    Correction bug de dimensions + Uniformisation de style

--- MRU.py
+++ MRU.py
@@ -8,30 +8,32 @@ from numpy.random import randn
 ##############################
 # FONCTIONS
 ##############################
-
-def w_mru(sigma, T_ech, n1, n2) :
+def w_mru(sigma, Tech, size) :
     """ 
         Génération d'un bruit blanc gaussien centré 
         représentant un bruit de modèle
     """
-    Q = sigma*np.array(([T_ech**3/3, T_ech**2/2],
-                        [T_ech**2/2, T_ech]))
-    return Q@randn(n1, n2) 
+    Q = sigma * np.array(([Tech**3/3, Tech**2/2],
+                        [Tech**2/2, Tech]))
+
+    return Q @ randn(*size) 
 
 ###############################
 # CALCUL MRU
 ###############################
-N = 500 # taille échantillon
-n1 = 2; n2 = 1
-sigma = 1 # Variance bbgc
-T_ech = 1 # Temps d'échantillonnage en seconde
-w = np.zeros([n1,N])
+N = 500         # Taille échantillon
+n1 = 2; n2 = 1  # Dimensions d'un échantillon
+sigma = 1       # Variance bbgc
+Tech = 1        # Temps d'échantillonnage en seconde
+
+w = np.zeros((n1,N))
 for i in range(N) :
-    w[:,i] = w_mru(sigma, T_ech, n1, n2)
+    w[:,i] = w_mru(sigma, Tech, (n1, n2))[:, 0]
     print(w[:,i])
 
 print(np.mean(w[0, :]))
 print(np.mean(w[1, :]))
+
 ###############################
 # AFFICHAGE
 ###############################
