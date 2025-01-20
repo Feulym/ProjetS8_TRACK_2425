471762238ba15a0f02096c6a2814bdb9bb5634d2
Sami Belfkira <sami.belfkira@bordeaux-inp.fr>
2025-01-20 11:03:23 +0100

    La fonction de génération d'une trajectoire MUA bruitée

---
+++ MUA.py
@@ -0,0 +1,50 @@
+###############################
+# IMPORTS
+###############################
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.random import randn
+
+##############################
+# FONCTIONS
+##############################
+def Trajec_MUA(N, Tech, sigma):
+    A = np.array([
+        [1, Tech, Tech**2/2],
+        [0, 1, Tech],
+        [0, 0, 1]
+    ])
+    
+    Q = sigma**2 * np.array([
+        [Tech**5/20, Tech**4/8, Tech**3/6],
+        [Tech**4/8, Tech**3/3, Tech**2/2],
+        [Tech**3/6, Tech**2/2, Tech]
+    ])
+    
+    L = np.linalg.cholesky(Q)
+    
+    X = np.zeros((3, N+1))
+
+    for k in range(N):
+        w = L @ np.random.randn(3, 1)
+        X[:, k+1] = A @ X[:, k] + w[:,0]
+    
+    t = np.arange(0, (N+1)*Tech, Tech)
+    return t, X
+
+# Test 
+N = 500
+Tech = 1
+sigma = 3
+
+t, X = Trajec_MUA(N, Tech, sigma)
+
+fig, axs = plt.subplots(3, 1, figsize=(10, 8))
+labels = ['Position', 'Vitesse', 'Accélération']
+for i in range(3):
+    axs[i].plot(t, X[i, :])
+    axs[i].set_title(labels[i])
+    axs[i].grid(True)
+
+plt.tight_layout()
+plt.show()
