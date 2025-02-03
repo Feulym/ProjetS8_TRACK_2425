ff6d8898cd342f676e44d6e1ebfa7b55d4fd1f88
DERVIEUX Milo <Milo.Dervieux@bordeaux-inp.fr>
2025-02-03 14:30:36 +0100

    Modification CR.tex et MRU.py datant de la séance de vendredi dernier

--- src/MRU.py
+++ src/MRU.py
@@ -9,7 +9,7 @@ import package.common as com
 ##############################
 # FONCTIONS
 ##############################
-def trajec_MRU(N, Tech, sigma) :
+def trajec_MRU(N, Tech, sigma, DEBUGSIGMA=False) :
     """ 
         Génération d'une trajectoire MRU 
         d'après un bruit blanc gaussien centré 
@@ -20,7 +20,7 @@ def trajec_MRU(N, Tech, sigma) :
     A = np.array(([1, Tech],
                   [0, 1]))
 
-    Q = sigma**2 * np.array([
+    Q = sigma * np.array([
         [Tech**3/3, Tech**2/2],
         [Tech**2/2, Tech     ]
     ])
@@ -28,17 +28,25 @@ def trajec_MRU(N, Tech, sigma) :
     D = np.linalg.cholesky(Q)
     
     X = np.zeros((2, N))
-
+    sum_ = 0
+    
     for k in range(N-1):
         w = D @ randn(2, 1)
+        sum_ += w @ w.T
         X[:, k+1] = A @ X[:, k] + w[:,0]
-           
+    
+    sum_ /= N-1;
+    if DEBUGSIGMA:
+        print("Q : ", Q)
+        print("Variance W : ", sum_)
+        print("Différence :", Q - sum_)
+    
     return t, X
 
 ###############################
 # CALCUL MRU
 ###############################
-N = 50        # Taille échantillon
+N = 5000        # Taille échantillon
 sigma2 = 3     # Variance bbgc
 Tech = 1      # Temps d'échantillonnage en seconde
 M = 20        # Nombre de réalisation
