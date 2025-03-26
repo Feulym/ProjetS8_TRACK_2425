9446ef228efc2063214bdd2cce08f4f42e68f9f1
DERVIEUX Milo <Milo.Dervieux@bordeaux-inp.fr>
2025-03-26 12:38:01 +0100

    Problème du bruit résolu : Ajout d'une fonction calculant l'écart de distance moyen entre 2 points pour un vecteur + ajout du bruit basé sur cet écart

--- src/data_gen/gen_data.sh
+++ src/data_gen/gen_data.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-durations=("1500 3600")
+durations=("120 300")
 sigma_loi=(0.01 0.1)
 tau=(60 40 5)
 sigma_m2=(0.03849 8.898 24.06)
+noised=1 # 1 if True, 0 if False
 
 data_dir="data"
 mkdir -p "$data_dir"
@@ -16,9 +17,9 @@ for duration in "${durations[@]}"; do
             sm2="${sigma_m2[$i]}"
             output_file="$data_dir/data_min${min_N}_max${max_N}_sigma${sigma}_tau${t}_sigmaM${sm2}.h5"
 
-            python src/lstm/data_gen.py \
+            python3 data_gen.py \
                 --output_file "$output_file" \
-                --n_samples 20000 \
+                --n_samples 1 \
                 --min_N "$min_N" \
                 --max_N "$max_N" \
                 --Tech 1.0 \
@@ -27,7 +28,8 @@ for duration in "${durations[@]}"; do
                 --min_tau "$t" \
                 --max_tau "$t" \
                 --min_sigma_m2 "$sm2" \
-                --max_sigma_m2 "$sm2"
+                --max_sigma_m2 "$sm2" \
+                --noised "$noised"
         done
     done
 done
